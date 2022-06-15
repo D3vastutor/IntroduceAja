f6c095e50fa153b5cb32c8e534383ffc068eac65
machtalNuar <devwasd@gmail.com>
2022-06-15 20:45:17 +0700

    + Login Page

---
+++ IntroduceAja/IntroduceAja/LoginPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IntroduceAja
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
